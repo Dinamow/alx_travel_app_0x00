e36c5625788581e14e01da16a593df98c6a43df3
DINAMOW <meemoo102039@gmail.com>
2025-03-06 01:27:53 +0200

    Refactors serializers for Listings and Bookings
    Updates serializers to define specific fields instead of using '__all__'.
    This improves control over data exposure and adheres to security best practices.
    
    Also, adjusts import statement for models.

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,15 +1,14 @@
 from rest_framework import serializers
-from listings.models import Listing, Booking
+from .models import Listing, Booking
 
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = '__all__'
-        read_only_fields = ['property_id', 'created_at', 'updated_at', 'id']
-
+        fields = ['id', 'title', 'description', 'price_per_night', 'location', 'created_at']
+        read_only_fields = ['property_id', 'created_at', 'updated_at']
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = '__all__'
-        read_only_fields = ['booking_id', 'created_at', 'id']
+        fields = ['id', 'user', 'listing', 'check_in', 'check_out', 'total_price']
+        read_only_fields = ['booking_id', 'created_at']
